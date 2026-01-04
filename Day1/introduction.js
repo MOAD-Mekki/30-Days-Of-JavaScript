f170d5e3cc95d6efc735171f8e046f9502a8330f
MOAD Mekki <moadahmedabdesselammekki@gmail.com>
2026-01-04 19:45:54 +0100

    Day1: Introduction and JS Basics

---
+++ Day1/introduction.js
@@ -0,0 +1,12 @@
+// comments can make code readable
+// Welcome to 30 Days of JavaScript
+/*
+Comments can make code readable,
+easy to reuse and informative
+*/
+console.log('Welcome to 30 Days of JavaScript');
+console.log('30 Days of JavaScript');
+let myAge = 18;
+let yourAge = 25;
+console.log('I am ' + myAge + ' years old.');
+console.log('You are ' + yourAge + ' years old.');
---
+++ Day1/variables.js
@@ -0,0 +1,7 @@
+let firstName = 'Asabeneh';
+let lastName = 'Yetayeh';
+let country = 'Finland';
+let city = 'Helsinki';
+let age = 250;
+let isMarried = true;
+let year = 2021;
